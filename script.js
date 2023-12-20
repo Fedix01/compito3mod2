1409365adb00f0d2e36cae52ab2b41478c7260ea
“Federico <federicomanna01@yahoo.it>
2023-12-20 22:09:17 +0100

    addedContent

---
+++ script.js
@@ -0,0 +1,12 @@
+// let myBtn = document.querySelectorAll(".btn");
+// console.log(myBtn);
+// let content = document.querySelector(".container");
+
+
+// myBtn.forEach(element => {
+//     element.addEventListener("click", () => {
+//         content.style.cssText = "display:none !important"
+//     })
+// });
+
+
